f1140fc89f8a442a86175d016544839897310cf4
Jack Madden <68071086+jackmadden246@users.noreply.github.com>
2023-01-11 16:47:20 +0000

    Amended code to overcome Lambda memory issues

--- Order_data_lambda_function.py
+++ Order_data_lambda_function.py
@@ -1,43 +1,29 @@
 import sqlalchemy
-import pandas as pd
 import pymysql
-import boto3
-import json
 from sqlalchemy import exc
+import dask.dataframe as dd
 
-
-def lambda_handler(event, context):
-        s3 = boto3.client("s3")
-        bucket = "mystaticwebsite5"
-        key = "order_data_20230106_1530.csv"
-        file = s3.get_object(Bucket=bucket, Key=key)
-        df = pd.read_csv(file['Body'])
-        df["Order Date"] = df["Order Date"].astype('datetime64')
-        df['Order Date'] = df['Order Date'].dt.date
-        df['Delivery Postcode'] = df['Delivery Postcode'].str.replace("%", " ")
-        df.drop(["is_first"], axis = 1, inplace = True)
-        df["Delivery Date"] = df["Delivery Date"].astype('datetime64')
-        df['Delivery Date'] = df['Delivery Date'].dt.date
-        df['Delivery Status'] = df['Delivery Status'].fillna(value= 'Unknown')
-        df['Delivery Date'] = df['Delivery Date'].fillna(value = '2022/10/02')
-        df['Dispatched Date'] = df['Dispatched Date'].astype('datetime64')
-        df['Dispatched Date'] = df['Dispatched Date'].dt.date
-        df.rename(columns={'Dispatched Date': 'Dispatch_Date', 'Order Date': 'Order_Date',
+df = dd.read_csv("/home/ec2-user/s3fs-fuse/mystaticwebsite5/order_data.csv")
+df["Order Date"] = df["Order Date"].astype('datetime64')
+df['Order Date'] = df['Order Date'].dt.date
+df['Delivery Postcode'] = df['Delivery Postcode'].str.replace("%", " ")
+df = df.drop(["is_first"], axis=1)
+df["Delivery Date"] = df["Delivery Date"].astype('datetime64')
+df['Delivery Date'] = df['Delivery Date'].dt.date
+df['Delivery Status'] = df['Delivery Status'].fillna(value='Unknown')
+df['Delivery Date'] = df['Delivery Date'].dropna()
+df['Dispatched Date'] = df['Dispatched Date'].astype('datetime64')
+df['Dispatched Date'] = df['Dispatched Date'].dt.date
+df = df.rename(columns={'Dispatched Date': 'Dispatch_Date', 'Order Date': 'Order_Date',
                            'Delivery Postcode': 'Delivery_Postcode', 'Order Number': 'Order_Number',
                            'Toothbrush Type': 'Toothbrush_Type', 'Customer Age': 'Customer_Age',
                            'Order Quantity': 'Order_Quantity','Billing Postcode': 'Billing_Postcode',
                            'Delivery Date': 'Delivery_Date', 'Delivery Status': 'Delivery_Status',
-                           'Dispatch Status': 'Dispatch_Status'}, inplace=True)
-        maria_db_details = "mysql+pymysql://admin:Titanic55!@database-10.cvunj1yhv8uv.eu-west-2.rds.amazonaws.com:3306/Toothdata1"
-        engine = sqlalchemy.create_engine(maria_db_details, echo=True)
-        for i in range (len(df)):
-                try:
-                        df.iloc[i:i + 1].to_sql(name='current_orders', con=engine, if_exists='append', index = 'id')
-                except exc.IntegrityError:
-                        # Ignore duplicates
-                        pass
-    
-        return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+                           'Dispatch Status': 'Dispatch_Status'})
+maria_db_details = "mysql+pymysql://admin:Titanic55!@database-10.cvunj1yhv8uv.eu-west-2.rds.amazonaws.com:3306/Toothdata1"
+engine = sqlalchemy.create_engine(maria_db_details, echo=True)
+try:
+        df.to_sql(name='current_orders', uri = maria_db_details, if_exists='append', chunksize = 1000, engine_kwargs = None, index='id')
+except exc.IntegrityError:
+        # Ignore duplicates
+        pass

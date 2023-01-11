c86871efbc2f30a3b8c6635bc8fda918db7aa837
Jack Madden <68071086+jackmadden246@users.noreply.github.com>
2023-01-11 23:13:04 +0000

    Amended code to remove any null values

--- all_orders.py
+++ all_orders.py
@@ -8,8 +8,11 @@ df = dd.read_csv("/home/ec2-user/s3fs-fuse/mystaticwebsite5/open_postcode_geo.cs
 df = df.drop(['usertype', 'easting', 'northing', 'positional_quality_indicator', 'postcode_no_space', 'postcode_fixed_width_seven', 'postcode_fixed_width_eight', 'postcode_area', 'postcode_district', 'postcode_sector', 'outcode', 'incode'], axis = 1)
 df = df.loc[df['status'] == 'live']
 df['latitude'] = df['latitude'].replace("'\\\\N'", 'NULL')
+df['latitude'] = df['latitude'].replace('\\N', 0.0)
+df['latitude'] = df['latitude'].fillna(value=0.0)
 df['longitude'] = df['longitude'].replace("'\\\\N'", 'NULL')
-
+df['longitude'] = df['longitude'].fillna(value=0.0)
+df['longitude'] = df['longitude'].replace('\\N', 0.0)
 maria_db_details = "mysql+pymysql://admin:Titanic55!@database-10.cvunj1yhv8uv.eu-west-2.rds.amazonaws.com:3306/Toothdata1"
 engine = sqlalchemy.create_engine(maria_db_details, echo=True)
 try:

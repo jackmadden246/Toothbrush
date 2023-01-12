648a035bf2a9553aa6e9dd2d9b50ae5fa89119ec
Jack Madden <68071086+jackmadden246@users.noreply.github.com>
2023-01-12 16:41:09 +0000

    Updated code to amend data cleaning issue

--- null_orders_lambda_function.py
+++ null_orders_lambda_function.py
@@ -15,20 +15,16 @@ def lambda_handler(event, context):
     df["Order Date"] = df["Order Date"].astype('datetime64')
     df['Order Date'] = df['Order Date'].dt.date
     df['Delivery Postcode'] = df['Delivery Postcode'].astype(str)
-    df['Delivery Postcode'] = df['Delivery Postcode'].str.replace("%", " ")
+    df['Delivery Postcode'] = df['Delivery Postcode'].str.replace("%20", " ")
     df['Delivery Postcode'] = df['Delivery Postcode'].str.upper()
-    df['Delivery Postcode'] = (df['Delivery Postcode'].str.replace(whitespace_re, ' ')).where( df['Delivery Postcode'].str.contains(whitespace_re),
-                                                                                              df['Delivery Postcode'])
-    df['Billing Postcode'] = df['Billing Postcode'].astype(str)
-    df['Billing Postcode'] = (df['Billing Postcode'].str.upper()).where(df['Billing Postcode'].str.islower(), df['Billing Postcode'])
-    df['Billing Postcode'] = df['Billing Postcode'].str[:4] + ' ' + df['Billing Postcode'].str[4:]
+    df['Delivery Postcode'] = (df['Delivery Postcode'].str.replace(whitespace_re, ' ')).where( df['Delivery Postcode'].str.contains(whitespace_re), df['Delivery Postcode'])
+    df['Billing Postcode'] = df['Billing Postcode'].str.upper().where(df['Billing Postcode'].str.islower(), df['Billing Postcode'])
+    df['Delivery Postcode'] = df['Delivery Postcode'].str.upper().where(df['Delivery Postcode'].str.islower(), df['Delivery Postcode'])
+    df['Billing Postcode'] = (df['Billing Postcode'].str.replace(whitespace_re, ' ')).where( df['Billing Postcode'].str.contains(whitespace_re), df['Billing Postcode'])
     df["Dispatched Date"] = df["Dispatched Date"].astype('datetime64')
     df['Dispatched Date'] = df['Dispatched Date'].dt.date
     df.drop(["is_first", "Delivery Status", "Delivery Date"], axis=1, inplace=True)
-    df.rename(columns ={'Dispatched Date':'Dispatch_Date', 'Order Date': 'Order_Date' , 'Delivery Postcode': 'Delivery_Postcode', 'Order Number': 'Order_Number', 
-                        'Toothbrush Type': 'Toothbrush_Type', 'Customer Age': 'Customer_Age', 'Order Quantity':'Order_Quantity', 
-                        'Delivery Postcode' : 'Delivery_Postcode', 
-                        'Billing Postcode': 'Billing_Postcode', 'Dispatch Status': 'Dispatch_Status'}, inplace=True)
+    df.rename(columns ={'Dispatched Date':'Dispatch_Date', 'Order Date': 'Order_Date' , 'Delivery Postcode': 'Delivery_Postcode', 'Order Number': 'Order_Number', 'Toothbrush Type': 'Toothbrush_Type', 'Customer Age': 'Customer_Age', 'Order Quantity':'Order_Quantity', 'Delivery Postcode' : 'Delivery_Postcode', 'Billing Postcode': 'Billing_Postcode', 'Dispatch Status': 'Dispatch_Status'}, inplace=True)
     maria_db_details = "mysql+pymysql://admin:Titanic55!@database-10.cvunj1yhv8uv.eu-west-2.rds.amazonaws.com:3306/Toothdata1"
     engine = sqlalchemy.create_engine(maria_db_details, echo=True)
     try:
